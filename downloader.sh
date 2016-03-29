d8ab81b89cf125aac87a37c365f36739f77e0593
Yamen Saraiji <mrayyamen@gmail.com>
2016-03-29 15:04:25 +0900

    first commit

--- downloader.sh
+++ downloader.sh
@@ -2,3 +2,7 @@ wget clone https://raw.githubusercontent.com/mrayy/WASABI/master/GIO.py
 wget clone https://raw.githubusercontent.com/mrayy/WASABI/master/armController.py
 wget clone https://raw.githubusercontent.com/mrayy/WASABI/master/headController.py
 wget clone https://raw.githubusercontent.com/mrayy/WASABI/master/pepperStreamer.sh
+wget clone https://raw.githubusercontent.com/mrayy/WASABI/master/run.sh
+
+chmod +x pepperStreamer.sh
+chmod +x run.sh
--- pepperStreamer.sh
+++ pepperStreamer.sh
@@ -2,7 +2,7 @@ echo streaming to $1
 
  gst-launch-0.10 -v \
  v4l2src always-copy=false queue-size=5 device=/dev/video0 ! 'video/x-raw-yuv,width=640,height=480,framerate=30/1' ! \
- jpegenc quality=70 ! rtpjpegpay ! udpsink host=$1 port=9000  sync=false -v
+ jpegenc quality=70 ! rtpjpegpay ! udpsink host=$1 port=9000  sync=false -v \
  alsasrc ! audioconvert ! audioresample ! alawenc ! rtppcmapay ! udpsink host=$1 port=12345 sync=false \
  udpsrc port=9001 ! application/x-rtp,media=audio,clock-rate=48000,encoding-name=VORBIS,payload=96 ! \
                    rtpvorbisdepay ! vorbisdec ! volume volume=1 ! autoaudiosink sync=false
